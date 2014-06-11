a6859836bc796803e99672fab6cc95e506c7f5ef
evgenlit <tcifrovik@gmail.com>
2014-06-11 17:01:59 +0400

    hjihj

--- application/views/layouts/index.php
+++ application/views/layouts/index.php
@@ -9,17 +9,19 @@
 		<?php endif; ?>
     </head>
     <body>
-	<div class="header">
-		<img id="picture" src="/res/main.jpg" width="810" height="200" />
-	</div>
-		<div class="sidebar">
-			<h2>Меню</h2>
-			<ul>
-				<li><a href="link1.html">Ссылка 1</a></li>
-				<li><a href="link2.html">Ссылка 2</a></li>
-				<li><a href="link3.html">Ссылка 3</a></li>
-				<li><a href="link4.html">Ссылка 4</a></li>
-			</ul>
+		<div class="header">
+			<img id="picture" src="/res/main.jpg" width="810" height="200" />
+		</div>
+		<div id="osnova">
+			<div id="obertka">
+				<ul class="css-menu-1">
+					<li><a href="#">Главная</a></li>
+					<li><a href="#">HTML уроки</a></li>
+					<li><a href="#">CSS уроки</a></li>
+					<li><a href="#" class="selected">CSS меню</a></li>
+					<li><a href="#">Справочник</a></li>
+				</ul>
+			</div>
 		</div>
         <div id="container">
             <div class="content">
